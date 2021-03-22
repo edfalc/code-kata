5395b3fce1ed42da8eafa8ab1ee22eda5b05c292
Eduardo Vieira Falcao <eduardo.vieira.falcao@ict.nl>
2021-03-22 09:55:26 +0100

    refactoring of code and test cases

--- arabic-to-roman/tests/ArabicToRomanNumeralsConverterTestCase.cpp
+++ arabic-to-roman/tests/ArabicToRomanNumeralsConverterTestCase.cpp
@@ -12,16 +12,27 @@ int main(int argc, char** argv) {
 	return RUN_ALL_TESTS();
 }
 
+struct ArabicToRomanMapping {
+	unsigned int arabicNumber;
+	std::string romanNumeral;
+};
+
+const std::size_t numberOfMappings { 3 };
+using ArabicToRomanMappings = std::array<ArabicToRomanMapping, numberOfMappings>;
+const ArabicToRomanMappings arabicToRomanMappings = { {
+		{100, "C"},
+		{ 10, "X"},
+		{  1, "I"}
+} };
+
 std::string convertArabicNumberToRomanNumeral (unsigned int arabicNumber) {
 	std::string romanNumeral;
 
-	while (arabicNumber >= 10) {
-		romanNumeral += "X";
-		arabicNumber -= 10;
-	}
-	while(arabicNumber >= 1){
-		romanNumeral += "I";
-		--arabicNumber;
+	for (const auto& mapping : arabicToRomanMappings) {
+		while (arabicNumber >= mapping.arabicNumber) {
+			romanNumeral += mapping.romanNumeral;
+			arabicNumber -= mapping.arabicNumber;
+		}
 	}
 
 	return romanNumeral;
@@ -51,4 +62,7 @@ TEST(ArabicToRomanNumeralsConverterTestCase, ConversionOfArabicNumbersToRomanNum
 	assertThat(20).isConvertedToRomanNumeral("XX");
 	assertThat(30).isConvertedToRomanNumeral("XXX");
 	assertThat(33).isConvertedToRomanNumeral("XXXIII");
+	assertThat(100).isConvertedToRomanNumeral("C");
+	assertThat(200).isConvertedToRomanNumeral("CC");
+	assertThat(300).isConvertedToRomanNumeral("CCC");
 }
