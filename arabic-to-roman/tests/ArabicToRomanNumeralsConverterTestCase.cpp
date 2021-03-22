afa291cfa4aaa5c0b0bec8b8bf9a39f70cd76678
Eduardo Vieira Falcao <eduardo.vieira.falcao@ict.nl>
2021-03-22 10:13:10 +0100

    added test cases

--- arabic-to-roman/tests/ArabicToRomanNumeralsConverterTestCase.cpp
+++ arabic-to-roman/tests/ArabicToRomanNumeralsConverterTestCase.cpp
@@ -17,12 +17,22 @@ struct ArabicToRomanMapping {
 	std::string romanNumeral;
 };
 
-const std::size_t numberOfMappings { 3 };
+const std::size_t numberOfMappings { 13 };
 using ArabicToRomanMappings = std::array<ArabicToRomanMapping, numberOfMappings>;
 const ArabicToRomanMappings arabicToRomanMappings = { {
-		{100, "C"},
-		{ 10, "X"},
-		{  1, "I"}
+		{ 1000, "M" },
+		{ 900, "CM" },
+		{ 500, "D" },
+		{ 400, "CD" },
+		{ 100, "C" },
+		{ 90, "XC" },
+		{ 50, "L" },
+		{ 40, "XL" },
+		{ 10, "X" },
+		{ 9, "IX" },
+		{ 5, "V" },
+		{ 4, "IV" },
+		{ 1, "I" }
 } };
 
 std::string convertArabicNumberToRomanNumeral (unsigned int arabicNumber) {
@@ -58,11 +68,26 @@ TEST(ArabicToRomanNumeralsConverterTestCase, ConversionOfArabicNumbersToRomanNum
 	assertThat(1).isConvertedToRomanNumeral("I");
 	assertThat(2).isConvertedToRomanNumeral("II");
 	assertThat(3).isConvertedToRomanNumeral("III");
+	assertThat(4).isConvertedToRomanNumeral("IV");
+	assertThat(5).isConvertedToRomanNumeral("V");
+	assertThat(6).isConvertedToRomanNumeral("VI");
+	assertThat(9).isConvertedToRomanNumeral("IX");
 	assertThat(10).isConvertedToRomanNumeral("X");
 	assertThat(20).isConvertedToRomanNumeral("XX");
 	assertThat(30).isConvertedToRomanNumeral("XXX");
 	assertThat(33).isConvertedToRomanNumeral("XXXIII");
+	assertThat(37).isConvertedToRomanNumeral("XXXVII");
+	assertThat(50).isConvertedToRomanNumeral("L");
+	assertThat(99).isConvertedToRomanNumeral("XCIX");
 	assertThat(100).isConvertedToRomanNumeral("C");
 	assertThat(200).isConvertedToRomanNumeral("CC");
 	assertThat(300).isConvertedToRomanNumeral("CCC");
+	assertThat(499).isConvertedToRomanNumeral("CDXCIX");
+	assertThat(500).isConvertedToRomanNumeral("D");
+	assertThat(1000).isConvertedToRomanNumeral("M");
+	assertThat(2000).isConvertedToRomanNumeral("MM");
+	assertThat(2017).isConvertedToRomanNumeral("MMXVII");
+	assertThat(3000).isConvertedToRomanNumeral("MMM");
+	assertThat(3333).isConvertedToRomanNumeral("MMMCCCXXXIII");
+	assertThat(3999).isConvertedToRomanNumeral("MMMCMXCIX");
 }
